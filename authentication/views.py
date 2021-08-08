cf4f0f72674f4e9c79913fa724229ff845623cce
kishanshetty1991 <nishithleo@yahoo.com>
2021-08-09 00:16:52 +0530

    Ajax Validation

--- authentication/views.py
+++ authentication/views.py
@@ -1,8 +1,39 @@
 from django.shortcuts import render
 from django.views import View
+import json
+from django.http import JsonResponse
+from django.contrib.auth.models import User
+from validate_email import validate_email
 
 # Create your views here.
 
+class EmailValidationView(View):
+    def post(self, request):
+        data = json.loads(request.body)
+        email = data['email']
+        if not validate_email(email):
+            return JsonResponse({'email_error':'Email is Invalid'},status=400);
+        if User.objects.filter(email=email).exists():
+            return JsonResponse({'email_error':'Email already in use,please choose another one '},status=409);    
+        return JsonResponse({'email_valid': True})
+
+
+
+class UsernameValidationView(View):
+    def post(self, request):
+        data = json.loads(request.body)
+        username = data['username']
+        if not str(username).isalnum():
+            return JsonResponse({'username_error':'Username should only contain Alphanumeric characters '},status=400);
+        if User.objects.filter(username=username).exists():
+            return JsonResponse({'username_error':'Username already in use,please choose another one '},status=409);    
+        return JsonResponse({'username_valid': True})
+
+
+
+
+
+
 class RegistrationView(View):
-	def get(self, request):
-		return render(request, 'authentication/register.html')
+    def get(self, request):
+        return render(request, 'authentication/register.html')
---
+++ expenseswebsite/static/js/register.js
@@ -0,0 +1,98 @@
+const usernameField = document.querySelector('#usernameField');
+const feedBackArea = document.querySelector('.invalid-feedback');
+const emailField = document.querySelector('#emailField');
+const passwordField = document.querySelector('#passwordField');
+const emailFeedBackArea = document.querySelector('.emailFeedBackArea');
+const usernameSuccessOutput = document.querySelector('.usernameSuccessOutput');
+const emailSuccessOutput = document.querySelector('.emailSuccessOutput');
+const showPasswordToggle = document.querySelector('.showPasswordToggle');
+
+
+const handleToggleInput = (e) => {
+	if(showPasswordToggle.textContent === "SHOW"){
+		showPasswordToggle.textContent = "HIDE";
+		passwordField.setAttribute("type","text");
+	}
+	else{
+		showPasswordToggle.textContent = "SHOW";
+		passwordField.setAttribute("type","password");
+
+	}
+}
+
+
+showPasswordToggle.addEventListener("click", handleToggleInput)
+
+
+
+
+
+
+
+emailField.addEventListener("keyup", (e) => {
+
+	const emailVal = e.target.value;
+
+	emailSuccessOutput.style.display = 'block';
+
+
+	emailSuccessOutput.textContent = `Checking ${emailVal}`
+
+	emailField.classList.remove("is-invalid");
+	emailFeedBackArea.style.display = "none"; 
+
+
+
+	if(emailVal.length > 0) {
+
+	fetch('/authentication/validate-email',{
+		body: JSON.stringify({ 'email': emailVal }),
+		method: "POST",
+	})
+	 .then(res => res.json())
+	 .then(data => {
+		console.log("data", data);
+		emailSuccessOutput.style.display = 'none';
+		if(data.email_error){
+			emailField.classList.add("is-invalid");
+			emailFeedBackArea.style.display = "block"; 
+			emailFeedBackArea.innerHTML = `<p>${data.email_error}</p>`
+		}
+	});
+	}
+});
+
+
+usernameField.addEventListener("keyup", (e) => {
+
+	const usernameVal = e.target.value;
+
+	usernameSuccessOutput.style.display = 'block';
+
+
+	usernameSuccessOutput.textContent = `Checking ${usernameVal}`
+
+
+	usernameField.classList.remove("is-invalid");
+	feedBackArea.style.display = "none"; 
+
+
+
+	if(usernameVal.length > 0) {
+
+	fetch('/authentication/validate-username',{
+		body: JSON.stringify({ 'username': usernameVal }),
+		method: "POST",
+	})
+	 .then(res => res.json())
+	 .then(data => {
+		console.log("data", data);
+		usernameSuccessOutput.style.display = 'none';
+		if(data.username_error){
+			usernameField.classList.add("is-invalid");
+			feedBackArea.style.display = "block"; 
+			feedBackArea.innerHTML = `<p>${data.username_error}</p>`
+		}
+	});
+	}
+});
