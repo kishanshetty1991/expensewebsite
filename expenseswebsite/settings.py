0a0c553c524b437d30188a296aada0a293231d7a
kishanshetty1991 <nishithleo@yahoo.com>
2021-08-14 22:22:57 +0530

    Creating Dashboard and Pagination

--- expenseswebsite/settings.py
+++ expenseswebsite/settings.py
@@ -43,6 +43,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'expenses',
+    'authentication',
+    'userpreferences',
 ]
 
 MIDDLEWARE = [
@@ -121,7 +123,6 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
