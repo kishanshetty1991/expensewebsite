e24428640859b68f45583b2cacdd2ebf54576f02
kishanshetty1991 <nishithleo@yahoo.com>
2021-08-19 18:51:24 +0530

    Adding Searchbar changes to expenses dashboard

--- expenses/urls.py
+++ expenses/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from . import views
-
+from django.views.decorators.csrf import csrf_exempt
 
 
 urlpatterns = [
@@ -8,5 +8,6 @@ urlpatterns = [
     path('add-expense', views.add_expense, name="add-expenses"),
     path('edit-expense/<int:id>', views.expense_edit, name="expense-edit"),
     path('expense-delete/<int:id>', views.delete_expense, name="expense-delete"),
+    path('search-expenses', csrf_exempt(views.search_expenses), name="search-expenses"),
 
 ]
--- expenses/views.py
+++ expenses/views.py
@@ -4,23 +4,35 @@ from .models import Expense, Category
 from django.contrib import messages
 from django.contrib.auth.models import User
 from django.core.paginator import Paginator
+import json
+from django.http import JsonResponse
+from userpreferences.models import UserPreference
 
+def search_expenses(request):
+    if request.method == 'POST':
+        search_str = json.loads(request.body).get('searchText')
+        expenses = Expense.objects.filter(amount__istartswith=search_str,owner=request.user) | Expense.objects.filter(
+            date__istartswith=search_str,owner=request.user) | Expense.objects.filter(
+            description__icontains=search_str,owner=request.user) | Expense.objects.filter(
+            category__icontains=search_str,owner=request.user) 
+        data = expenses.values() 
+        return JsonResponse(list(data), safe=False) 
 
 # Create your views here.
 @login_required(login_url='/authentication/login')
 def index(request):
     categories = Category.objects.all()
     expenses = Expense.objects.filter(owner=request.user)
-    paginator = Paginator(expenses, 2)
+    paginator = Paginator(expenses, 5)
     page_number = request.GET.get('page')
     page_obj = Paginator.get_page(paginator, page_number)
-
+    currency = UserPreference.objects.get(user=request.user).currency
     context = {
         'expenses': expenses,
-        'page_obj': page_obj
+        'page_obj': page_obj,
+        'currency': currency
     }
-    categories = Category.objects.all()
-    return render(request,'expenses/index.html', context)
+    return render(request, 'expenses/index.html', context)
 
 @login_required(login_url='/authentication/login')
 def add_expense(request):
